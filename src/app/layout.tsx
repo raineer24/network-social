232ceb0d21e978dfe9fe3f02279ee21c73b4f6a9
raineer <raineerdelarita@gmail.com>
2025-01-28 23:27:38 +0800

    navbar tsx css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,12 +40,16 @@ export default function RootLayout({
           >
             <div className="min-h-screen">
               <Navbar />
-              <main>
-              {children}
+              <main className="py-8">
+                {/* container to center the content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid gird-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg-block lg:col-span-3">sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
               </main>
-              
             </div>
-           
           </ThemeProvider>
         </body>
       </html>
