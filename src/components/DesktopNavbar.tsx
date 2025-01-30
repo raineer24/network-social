a4c3ab077a10dbe9bf77aed4b6a3620f03c15794
raineer <raineerdelarita@gmail.com>
2025-01-30 14:03:23 +0800

    buttons

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,11 +1,39 @@
+import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import ModeToggle from "./ModeToggle";
 import { currentUser } from "@clerk/nextjs/server";
-
 async function DesktopNavbar() {
-    const user = await currentUser();
-    console.log('user', user);
+  const user = await currentUser();
+  console.log("user", user);
   return (
-    <div>DesktopNavbar</div>
-  )
+    <div className="hidden md:flex items-center space-x-4">
+      <ModeToggle />
+
+      <Button variant="ghost" className="flex items-center gap-2" asChild>
+        <Link href='/'>
+            <HomeIcon className="w-4 h-4"/>
+            <span className="hidden lg:inline">Home</span>
+        </Link>
+      </Button>
+
+      {user ? (
+        <>
+        <Button>
+            <span>Notifications</span>
+        </Button>
+        <Button>
+            <span>Profile</span>
+        </Button>
+        </>
+      ): (
+        <SignInButton mode="modal">
+            <Button variant='default'>Sign In</Button>
+        </SignInButton>
+      )}
+    </div>
+  );
 }
 
 export default DesktopNavbar;
