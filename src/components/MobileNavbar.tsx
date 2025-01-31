bbf6db3951279a8fc6c4bffc0dd72c19b42a3fbf
raineer <raineerdelarita@gmail.com>
2025-01-31 14:40:29 +0800

    sheet button

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     BellIcon,
     HomeIcon,
@@ -14,18 +16,36 @@ import {
   import { useTheme } from "next-themes";
   import Link from "next/link";
 
-
-  import React from 'react'
   
   function MobileNavbar() {
+    const { theme, setTheme } = useTheme();
+
     return (
     <div className="flex md:hidden items-center space-x-2">
-        <Button variant='ghost' size='icon' className="mr-2">
-        <SunIcon />
-        <MoonIcon />
+        <Button variant='ghost' size='icon' 
+       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="mr-2">
+         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
         </Button>
         
+
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost">
+            <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile!</SheetTitle>
+         
+        </SheetHeader>
+      
+       
+      </SheetContent>
+    </Sheet>
     </div>
     )
   }
