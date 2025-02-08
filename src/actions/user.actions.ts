4b32bd92a1579c5907a76c4f9e60ad652d2590f3
raineer <raineerdelarita@gmail.com>
2025-02-08 23:17:17 +0800

    user actions ts

---
+++ src/actions/user.actions.ts
@@ -0,0 +1,10 @@
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function syncUser() {
+  return (
+   
+  )
+}
+
