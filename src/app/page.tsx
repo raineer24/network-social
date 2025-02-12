b88217126ca5717cf8f6ec63c1f4d4f434e8d5bc
raineer <raineerdelarita@gmail.com>
2025-02-12 11:59:55 +0800

    sticky top-20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const user = await currentUser();
   return (
-    <div>
-      <h1>home page content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div>
+        {user ? <CreatePost /> : null}
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
