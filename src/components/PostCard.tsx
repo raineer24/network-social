93c2624ea2702a364b20d8a9fdcf42f20a32ebe1
raineer <raineerdelarita@gmail.com>
2025-02-20 21:13:41 +0800

    return type post

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
 
-function PostCard() {
+"use client";
+import { getPosts, } from "@/actions/post.action";
+
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
   return (
     <div>PostCard</div>
   )
