39ca1978bf8dca0f85d283b08382eca661cf73d3
raineer <raineerdelarita@gmail.com>
2025-03-13 20:22:29 +0800

    profile

---
+++ src/actions/profile.action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { getDbUserId } from "./user.action";
+
+export async function getProfileByUsername(username: string) {
+    
+}
